13f8f68147568f863d06539c0765cb065e4d35e8
vinyshoow <viniciuslopes.qz@hotmail.com>
2020-08-19 09:05:40 -0300

    Finalizando a parte de cadastro de instrutores.

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const instructors = require('./instructors');
 
 
 const routes = express.Router();
@@ -10,6 +11,12 @@ routes.get('/', function(req, res) {
 routes.get('/instructors', function(req, res) {
   return res.render('instructors/index');
 })
+
+routes.get('/instructors/create', function(req, res) {
+  return res.render('instructors/create');
+})
+
+routes.post('/instructors', instructors.post);
 
 routes.get('/members', function(req, res) {
   return res.send('Members');
